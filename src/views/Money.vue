298319b9079db777fad2a8247b08cc150ac71ddc
lanycsq <shanqiong118@163.com>
2020-12-31 15:08:14 +0800

    money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,17 +1,12 @@
 <template>
-  <div class="container">
-    Money.vue
-    <hr />
+  <div class="nav-wrapper">
+    <div class="content">Money.vue</div>
     <Nav />
   </div>
 </template>
 
 <script>
-import Nav from "@/components/Nav.vue";
 export default {
-  components: {
-    Nav,
-  },
   data() {
     return {};
   },
@@ -19,4 +14,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
