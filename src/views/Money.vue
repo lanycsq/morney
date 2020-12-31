5bad39d22f48518cb47f0d525dd2666c1b9917e0
lanycsq <shanqiong118@163.com>
2020-12-31 12:15:28 +0800

    创建了三个页面

---
+++ src/views/Money.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">Money.vur</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">Statistics</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
+
