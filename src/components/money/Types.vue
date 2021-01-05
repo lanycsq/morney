651f1be6b6c1ac6c80d22edafa91d471e6f4e612
lanycsq <shanqiong118@163.com>
2021-01-05 15:45:32 +0800

    完成Types组件

---
+++ src/components/money/Types.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <ul class="types">
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+@Component
+export default class Types extends Vue {
+  type = "-";
+  selectType(type: string) {
+    if (type !== "-" && type !== "+") {
+      throw new Error("type is unknown");
+    }
+    this.type = type;
+  }
+}
+// export default {
+//   name: "Types",
+//   data() {
+//     return {
+//       type: "-", //"-"表示支出。"+" 表示收入
+//     };
+//   },
+//   methods: {
+//     selectType(type) {
+//       if (type !== "-" && type !== "+") {
+//         throw new Error("type is unknown");
+//       }
+//       this.type = type;
+//     },
+//   },
+// };
+</script>
+
+<style scoped lang="scss">
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      content: " ";
+      position: absolute;
+      bottom: 0px;
+      left: 0px;
+      height: 4px;
+      width: 100%;
+      background: #333;
+    }
+  }
+}
+</style>
