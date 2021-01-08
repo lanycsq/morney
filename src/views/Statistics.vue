b8f605780fe18edde2d416bd8a5d28ea99ef2a74
lanycsq <shanqiong118@163.com>
2021-01-08 16:18:30 +0800

    抽离tabs.vue

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,21 +1,45 @@
 
 <template>
-  <div>
-    <lay-out>
-      <p>Statistics</p>
-    </lay-out>
-  </div>
+  <lay-out>
+    <!-- <types class-prefix="x" :value.sync="yyy" /> -->
+    <tabs class-prefix="types" :data-source="array" :value.sync="yyy" />
+    <tabs class-prefix="interval" :data-source="array1" :value.sync="hhh" />
+  </lay-out>
 </template>
 
 <script  lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
 import LayOut from "@/components/LayOut.vue";
-export default {
-  components: { LayOut },
-  data() {
-    return {};
-  },
-};
+import Types from "@/components/money/Types.vue";
+import Tabs from "@/components/Tabs.vue";
+
+@Component({
+  components: { LayOut, Types, Tabs },
+})
+export default class Statistice extends Vue {
+  yyy = "-";
+  hhh = "day";
+  array = [
+    { text: "支出", value: "-" },
+    { text: "收入", value: "+" },
+  ];
+  array1 = [
+    { text: "按天", value: "day" },
+    { text: "按周", value: "week" },
+    { text: "按月", value: "month" },
+  ];
+}
 </script>
 <style scoped lang="scss">
+::v-deep .types-tabs-item {
+  background: white;
+  &.selected {
+    background: #c4c4c4;
+    &::after {
+      display: none;
+    }
+  }
+}
 </style>
 
